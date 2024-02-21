a3a05fdede5b9772481dff94e0a0338087b75aa2
Alexandra Côté-Lahue <alexcotelahue@gmail.com>
2024-02-21 00:28:38 -0500

    Add dark mode button functions

--- js/script.js
+++ js/script.js
@@ -52,6 +52,7 @@ for (i = 0; i < collapse.length; i++) {
 }
 
 // Slide-in animation
+
 document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', revealProjects);
 
@@ -70,5 +71,18 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// Dark mode button
 
+const themeSwitchInput = document.querySelector("#toggle");
+const ball = document.querySelector(".ball");
+const body = document.querySelector("body");
 
+themeSwitchInput.addEventListener("click", function () {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    ball.classList.remove("move-right");
+  } else {
+    body.classList.add("dark");
+    ball.classList.add("move-right");
+  }
+});
